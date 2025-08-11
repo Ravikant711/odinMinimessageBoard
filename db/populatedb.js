c1ce275b9f1854f6c57d242c40d240d46f97c68b
Ravikant711 <ravikantsingh711200@gmail.com>
2025-08-11 23:35:50 +0530

    Added render DATABASE_URL to connect render postgres db

--- db/populatedb.js
+++ db/populatedb.js
@@ -1,6 +1,21 @@
 require('dotenv').config();
 const { Client } = require('pg');
 
+// const SQL = `
+//   CREATE TABLE IF NOT EXISTS messageboard (
+//     id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+//     text TEXT NOT NULL,
+//     username VARCHAR(255) NOT NULL,
+//     added TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+//   );
+
+//   INSERT INTO messageboard (username, text)
+//   VALUES
+//     ('Ravi', 'Hello everyone!'),
+//     ('Niraj', 'Keep up the amazing work, team!');
+// `;
+
+//WHERE NOT EXISTS check → only inserts if that exact row doesn’t already exist
 const SQL = `
   CREATE TABLE IF NOT EXISTS messageboard (
     id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
@@ -10,17 +25,29 @@ const SQL = `
   );
 
   INSERT INTO messageboard (username, text)
-  VALUES
-    ('Ravi', 'Hello everyone!'),
-    ('Niraj', 'Keep up the amazing work, team!');
+  SELECT 'Ravi', 'Hello everyone!'
+  WHERE NOT EXISTS (
+    SELECT 1 FROM messageboard WHERE username = 'Ravi' AND text = 'Hello everyone!'
+  );
+
+  INSERT INTO messageboard (username, text)
+  SELECT 'Niraj', 'Keep up the amazing work, team!'
+  WHERE NOT EXISTS (
+    SELECT 1 FROM messageboard WHERE username = 'Niraj' AND text = 'Keep up the amazing work, team!'
+  );
 `;
 
 async function main() {
   console.log("seeding...");
-  const connectionString = `postgresql://postgres:${process.env.DB_PASSWORD}@localhost:${process.env.DB_PORT}/${process.env.DB_NAME}`;
+  // const connectionString = `postgresql://postgres:${process.env.DB_PASSWORD}@localhost:${process.env.DB_PORT}/${process.env.DB_NAME}`;
+
+  const connectionString = process.env.DATABASE_URL; // render connection string
 
   const client = new Client({
     connectionString,
+     ssl: {
+      rejectUnauthorized: false, // Required for Render Postgres
+    },
   });
   await client.connect();
   await client.query(SQL);
